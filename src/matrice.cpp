c411fe5be1ddc04e33ba824884f878dbf94071a0
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2023-10-07 15:53:09 -0400

    Refactor

--- src/matrice.cpp
+++ src/matrice.cpp
@@ -1,11 +1,13 @@
 #include "utils.h"
 
-#define ERREUR -1
+/****************************************/
+/**** CONSTANTES ****/
+/****************************************/
 
 // Pour faire avancer le robot
 const int PULSES_PAR_TOUR = 3200;
 const float VITESSE_AVANCER_MIN = 0.05;
-const float VITESSE_AVANCER_MAX = 0.6;
+const float VITESSE_AVANCER_MAX = 0.5;
 const float CIRCONFERENCE_ROUE_M = 0.239389;
 const float TAILLE_CELLULE = 0.475; // En metre
 
@@ -16,11 +18,12 @@ const float VITESSE_TOURNER_MAX = 0.3;
 
 // Pour les ajustement des vitesses des moteurs
 const int INTERVALLE_PRISE_MESURE = 100; // En ms
+const float PPM_TAUX_AJUSTEMENT_DISTANCE = 0.25; // Taux d'ajustement pour egaliser la distance parcourue totale des deux roues
 const float CORRECTION_MIN = 0.9;
 const float CORRECTION_MAX = 1.1;
 
 // Dimensions du parcours
-const int NB_COLONNES = 3;
+const int NB_COLS = 3;
 const int NB_LIGNES = 10;
 
 // Pins du robot
@@ -28,142 +31,106 @@ const int PIN_PROX_DROITE = 47; // Lumiere verte
 const int PIN_PROX_GAUCHE = 49; // Lumiere rouge
 const int PIN_MICRO = 0;
 
+
+/****************************************/
+/**** VARIABLES GLOBALES ****/
+/****************************************/
+
+int g_ligne;                        // Position y du robot
+int g_colonne;                      // Position x du robot
+int g_dir;                          // Direction cardinale du robot (0 = Nord, 1 = Est, 2 = Ouest, 3 = Sud)
+int g_matrice[NB_COLS][NB_LIGNES];  // Matrice des cellules du parcours (0 = Case non exploree, 1 = Case exploree)
+
+
+/****************************************/
+/**** FONCTIONS (DECLARATIONS) ****/
+/****************************************/
+
+void avanceDistance(float distance);
+void tourne(int dir);
+void changerDirection(int dir);
+void deplacerCellule(int dir);
+void reinitialiserEncodeurs();
+void arret();
+void beep(int count, int ms);
+void erreur(int beepCount);
+void succes();
+
 bool mur();
 bool sifflet();
-bool arrive(int ligne);
-void avanceDistance(float distance);
-void deplacerCellule(int dir, int& colonne, int& ligne);
-void arret();
-void tourne(int dir);
-void changerDirection(int& currDir, int newDir);
-void beep(int count, int ms);
-void reinitialiser_encodeurs();
-int ENCODER_Read_Ajuste(int id);
+bool arrive();
+bool verifierCase(int dir);
+
+
+/****************************************/
+/**** SETUP & LOOP ****/
+/****************************************/
 
 void setup() {
   BoardInit();
 }
 
 void loop() {
-  int ligne = NB_LIGNES - 1; // On commence du bas du tableau, donc index de la derniere ligne
-  int colonne = 1;
-  int dir = 0; // 0 = Nord, 1 = Est, 2 = Sud, 3 = Ouest
-  int matrice[NB_COLONNES][NB_LIGNES] = {0};
+  g_ligne = NB_LIGNES - 1;
+  g_colonne = 1;
+  g_dir = 0;
+  g_matrice[NB_COLS][NB_LIGNES] = {0};
 
   // while (!sifflet()); // On attend le signal du sifflet
 
-  matrice[colonne][ligne] = 1;
-
-  while (!arrive(ligne)) 
-  {
-    // Nord
-    if (ligne != 0 && matrice[colonne][ligne-1] == 0) {
-      changerDirection(dir, 0);
-      if (!mur()) {
-        avanceDistance(TAILLE_CELLULE);
-        ligne--;
-        matrice[colonne][ligne] = 1;
-        continue;
-      }
-    }
-    
-    // Est
-    if (colonne != NB_COLONNES - 1 && matrice[colonne+1][ligne] == 0) {
-      changerDirection(dir, 1);
-      if (!mur()) {
-        avanceDistance(TAILLE_CELLULE);
-        colonne++;
-        matrice[colonne][ligne] = 1;
-        continue;
-      }
-    }
-    
-    // Ouest
-    if (colonne != 0 && matrice[colonne-1][ligne] == 0) {
-      changerDirection(dir, 3);
-      if (!mur()) {
-        avanceDistance(TAILLE_CELLULE);
-        colonne--;
-        matrice[colonne][ligne] = 1;
-        continue;
+  while (!arrive())
+    // On verifie chaque direction pour voir si on peut s'y deplacer
+    for (int dir=0; dir <= 4; dir++) {
+      if (dir == 4)
+        erreur(5); // Erreur, aucune case valide, on quitte le programme
+      if (verifierCase(dir)) {
+        deplacerCellule(dir); // Si la case est non exploree et il n'y a pas de mur, on s'y deplace
+        break; // On recommence la verification a partir de la direction Nord (0)
       }
     }
 
-    // Sud
-    if (ligne != NB_LIGNES - 1 && matrice[colonne][ligne+1] == 0) {
-      changerDirection(dir, 2);
-      if (!mur()) {
-        avanceDistance(TAILLE_CELLULE);
-        ligne++;
-        matrice[colonne][ligne] = 1;
-        continue;
-      }
-    }
-  }
-
-  beep(1, 1000);
-  exit(EXIT_SUCCESS);
+  succes();
 }
 
-/********************************************************/
-// Fonctions
-/********************************************************/
 
-// Detecte s'il y a un mur devant le robot
-bool mur() {
-  return !digitalRead(PIN_PROX_GAUCHE) || !digitalRead(PIN_PROX_DROITE);
-}
-
-// Detecte le signal du sifflet
-bool sifflet() {
-  // Serial.print(analogRead(PIN_MICRO));
-  // Serial.print('\n');
-  return true;
-  return analogRead(PIN_MICRO) > 400;
-}
-
-// Detecte si le robot est arrivee a la derniere rangee
-bool arrive(int ligne) {
-  return ligne == NB_LIGNES - 1; // TODO
-}
+/****************************************/
+/**** FONCTIONS (DEFINITIONS) ****/
+/****************************************/
 
 // Avance d'une certaine distance en metres
-void avanceDistance(float distance)
-{
+void avanceDistance(float distance){
   // Converti la distance en metre au nb de pulse necessaire
   const int PULSES_A_PARCOURIR = PULSES_PAR_TOUR * (distance / CIRCONFERENCE_ROUE_M);
   const int PPM_VOULU_MIN = 20;       // Distance min a parcourir par mesure pour correction
-  const float PPM_TAUX_AJUSTEMENT_DISTANCE = 4.0; // Taux d'ajustement pour egaliser la difference de distance parcourue entre les deux roues
-                                                  // Plus le nombre est grand, plus l'ajustement est moindre
 
-  float vitesseG = VITESSE_AVANCER_MAX; // La vitesse de base des moteurs
-  float vitesseD = VITESSE_AVANCER_MAX;
+  float vitesseG = 0; // La vitesse de base des moteurs (sans correction)
+  float vitesseD = 0;
   int encodeurG = 0; // Lit le nombre de pulses des encodeurs
   int encodeurD = 0;
-  int pulsesParcourusG = 0; // Nombre total de pulses emits par les roues depuis le debut du mouvement
-  int pulsesParcourusD = 0;
-  int PPMvoulu = 0; // Pulses par mesure qu'on veut que les deux roues parcours (moyenne des 2 roues)
-  int PPMdiff = 0; // Difference entre les distances parcourues des deux roues
+  int distG = 0; // Distance totale en pulses parcourue par la roue
+  int distD = 0;
+  int PPMvoulu = 0; // Moyenne des 2 roues (Pulses par mesure)
+  int PPMdiff = 0; // Difference des distances totales parcourues par les deux roues
   float correctionG = 1; // Multiplicateur correctif de la vitesse du robot pour que les deux roues roulent a la meme vitesse
   float correctionD = 1;
 
   // Tant que le nombre de pulse necessaire pour faire la distance desire est plus petit que le compteur des encoders
-  while(pulsesParcourusG < PULSES_A_PARCOURIR || pulsesParcourusD < PULSES_A_PARCOURIR)
+  while(distG < PULSES_A_PARCOURIR || distD < PULSES_A_PARCOURIR)
   {
-    reinitialiser_encodeurs();
+    reinitialiserEncodeurs();
 
-    int x = (pulsesParcourusG + pulsesParcourusD) / 2; // Distance parcourue jusqu'a present
+    int x = (distG + distD) / 2; // Distance parcourue jusqu'a present
     vitesseG = vitesseD = (VITESSE_AVANCER_MAX - VITESSE_AVANCER_MIN) * sin((PI*x)/PULSES_A_PARCOURIR) + VITESSE_AVANCER_MIN;
 
-    MOTOR_SetSpeed(0, vitesseG * correctionG);
-    MOTOR_SetSpeed(1, vitesseD * correctionD);
+    MOTOR_SetSpeed(LEFT, vitesseG * correctionG);
+    MOTOR_SetSpeed(RIGHT, vitesseD * correctionD);
     delay(INTERVALLE_PRISE_MESURE);
 
-    pulsesParcourusG += encodeurG = ENCODER_Read(LEFT);   // Update du nb de pulse
-    pulsesParcourusD += encodeurD = ENCODER_Read(RIGHT);
+    distG += encodeurG = ENCODER_Read(LEFT);   // Update du nb de pulse
+    distD += encodeurD = ENCODER_Read(RIGHT);
 
     PPMvoulu = (encodeurG + encodeurD) / 2;
-    PPMdiff = (pulsesParcourusG - pulsesParcourusD) / PPM_TAUX_AJUSTEMENT_DISTANCE;
+    PPMdiff = (distG - distD) / PPM_TAUX_AJUSTEMENT_DISTANCE;
 
     // Ajuste la vitesse de chaque roue individuellement en fonction de la difference
     if (encodeurG > 0 && encodeurD > 0 && PPMvoulu > PPM_VOULU_MIN) {
@@ -179,43 +146,33 @@ void avanceDistance(float distance)
   arret();
 }
 
-// Fait arreter le robot
-void arret(){
-  MOTOR_SetSpeed(RIGHT, 0);
-  MOTOR_SetSpeed(LEFT, 0);
-  delay(100);
-}
-
 // Fait tourner le robot de 90deg a gauche (LEFT) ou droite (RIGHT)
 void tourne(int dir){
   const int PPM_VOULU_MIN = 20; // Distance min a parcourir par mesure pour correction
-  const float PPM_TAUX_AJUSTEMENT_DISTANCE = 3.5; // Taux d'ajustement pour egaliser la difference de distance parcourue entre les deux roues
-                                                  // Plus le nombre est grand, plus l'ajustement est moindre
-  int encodeurA = 0; // Avance
-  int encodeurR = 0; // Recule
-  int sumA = 0;
-  int sumR = 0;
-  float vitesseA = 0;
+
+  float vitesseA = 0; // La vitesse de base des moteurs (sans correction)
   float vitesseR = 0;
-  int PPMdiff = 0;  // Difference entre les distances parcourues des deux roues
-  int PPMvoulu = 0; // Pulse per measure
+  int encodeurA = 0; // Lit le nombre de pulses des encodeurs
+  int encodeurR = 0;
+  int distA = 0; // Distance totale en pulses parcourue par la roue
+  int distR = 0;
+  int PPMvoulu = 0; // Moyenne des 2 roues (Pulses par mesure)
+  int PPMdiff = 0; // Difference des distances totales parcourues par les deux roues
   float correctionA = 1; // Multiplicateur correctif de la vitesse du robot pour que les deux roues roulent a la meme vitesse
   float correctionR = 1;
-  int roueQuiAvance = ERREUR;
+  int roueQuiAvance = -1;
 
-  if (dir == LEFT)        roueQuiAvance = RIGHT;
-  else if (dir == RIGHT)  roueQuiAvance = LEFT;
-  
-  if (roueQuiAvance == ERREUR) {
-    beep(5, 250);
-    return;
+  switch (dir) {
+    case LEFT: roueQuiAvance = RIGHT; break;
+    case RIGHT: roueQuiAvance = LEFT; break;
+    default: erreur(6); // On quitte le programme, direction invalide
   }
 
-  while(sumA < PULSES_TOURNER_90_DEG || sumR > -PULSES_TOURNER_90_DEG)
+  while(distA < PULSES_TOURNER_90_DEG || distR > -PULSES_TOURNER_90_DEG)
   {
-    reinitialiser_encodeurs();
+    reinitialiserEncodeurs();
 
-    int x = (sumA - sumR) / 2; // Distance parcourue jusqu'a present
+    int x = (distA - distR) / 2; // Distance parcourue jusqu'a present
     vitesseA = (VITESSE_TOURNER_MAX - VITESSE_TOURNER_MIN) * sin((PI*x)/PULSES_TOURNER_90_DEG) + VITESSE_TOURNER_MIN;
     vitesseR = -vitesseA;
 
@@ -223,11 +180,11 @@ void tourne(int dir){
     MOTOR_SetSpeed(!roueQuiAvance, vitesseR * correctionR);
     delay(INTERVALLE_PRISE_MESURE);
 
-    sumA += encodeurA = ENCODER_Read(roueQuiAvance);   //update du nb de pulse
-    sumR += encodeurR = ENCODER_Read(!roueQuiAvance);
+    distA += encodeurA = ENCODER_Read(roueQuiAvance);   //update du nb de pulse
+    distR += encodeurR = ENCODER_Read(!roueQuiAvance);
 
     PPMvoulu = (encodeurA - encodeurR) / 2;
-    PPMdiff = (sumA + sumR) / PPM_TAUX_AJUSTEMENT_DISTANCE;
+    PPMdiff = (distA + distR) * PPM_TAUX_AJUSTEMENT_DISTANCE;
 
         // Ajuste la vitesse de chaque roue individuellement en fonction de la difference
     if (encodeurA > 0 && encodeurR < 0 && PPMvoulu > PPM_VOULU_MIN) {
@@ -243,17 +200,47 @@ void tourne(int dir){
   arret();
 }
 
-// 0 = Nord, 1 = Est, 2 = Sud, 3 = Ouest
-void changerDirection(int& currDir, int newDir) {
-  while (currDir != newDir) {
+// Fait tourner le robot jusqu'a ce qu'il fait face a la bonne direction
+void changerDirection(int dir) {
+  while (g_dir != dir) {
       tourne(RIGHT);
-      currDir = (currDir + 1) % 4;
+      switch (g_dir) {
+        case 0: g_dir = 1; break; // Nord  -> Est
+        case 1: g_dir = 3; break; // Est   -> Sud
+        case 2: g_dir = 0; break; // Ouest -> Nord
+        case 3: g_dir = 2; break; // Sud   -> Ouest
+      }
   }
+}
+
+// Deplace le robot vers la cellule devant celui-ci
+void deplacerCellule(int dir) {
+  avanceDistance(TAILLE_CELLULE);
+  switch (g_dir) {
+    case 0: g_ligne--;    break;
+    case 1: g_colonne++;  break;
+    case 2: g_colonne--;  break;
+    case 3: g_ligne++;    break;
+  }
+  g_matrice[g_colonne][g_ligne] = 1; // Marque la nouvelle case comme exploree
+}
+
+// Reinitialise les valeurs des encodeurs des moteurs
+void reinitialiserEncodeurs() {
+  ENCODER_ReadReset(LEFT);
+  ENCODER_ReadReset(RIGHT);
+}
+
+// Fait arreter le robot
+void arret(){
+  MOTOR_SetSpeed(RIGHT, 0);
+  MOTOR_SetSpeed(LEFT, 0);
+  delay(100);
 }
 
 // Fait beeper le robot un certain nombre de fois
 void beep(int count, int ms){
-  for(int i=0;i<count;i++) {
+  for(int i = 0; i < count; i++) {
     AX_BuzzerON();
     delay(ms);
     AX_BuzzerOFF();
@@ -261,7 +248,51 @@ void beep(int count, int ms){
   }
 }
 
-void reinitialiser_encodeurs() {
-  ENCODER_ReadReset(LEFT);
-  ENCODER_ReadReset(RIGHT);
+// Quitte le programme et emet le signal d'erreur
+void erreur(int beepCount) {
+  beep(beepCount, 100); // Erreur!
+  exit(EXIT_FAILURE);
+}
+
+// Quitte le programme et emet le signal de reussite du parcours
+void succes() {
+  beep(1, 1000);
+  exit(EXIT_SUCCESS);
+}
+
+// Detecte s'il y a un mur devant le robot
+bool mur() {
+  return !digitalRead(PIN_PROX_GAUCHE) || !digitalRead(PIN_PROX_DROITE);
+}
+
+// Detecte le signal du sifflet
+bool sifflet() {
+  return analogRead(PIN_MICRO) > 500;
+}
+
+// Detecte si le robot est arrivee a la derniere rangee
+bool arrive() {
+  return g_ligne == NB_LIGNES - 1;
+}
+
+// Verifie si la case dans la direction voulue existe et n'est pas exploree
+bool verifierCase(int dir) {
+  bool caseValide;
+
+  // On verifie d'abord que la cellule existe et qu'elle est non exploree
+  switch (dir) {
+    case 0:   caseValide = g_ligne  != 0            && g_matrice[g_colonne][g_ligne-1] == 0;
+    case 1:   caseValide = g_colonne != NB_COLS - 1 && g_matrice[g_colonne+1][g_ligne] == 0;
+    case 2:   caseValide = g_colonne != 0           && g_matrice[g_colonne-1][g_ligne] == 0;
+    case 3:   caseValide = g_ligne != NB_LIGNES - 1 && g_matrice[g_colonne][g_ligne+1] == 0;
+    default:  return false;
+  }
+
+  if (caseValide) {
+    // On regarde ensuite s'il y a un mur dans le chemin
+    changerDirection(dir);
+    caseValide = !mur();
+  }
+
+  return caseValide;
 }
